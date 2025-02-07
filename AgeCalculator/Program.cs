59c0b566bf1e693dd4aa5a7fb3737d088d996c40
Georgi Georgiev <Georgi Georgiev@DESKTOP-9FI4TKH>
2025-02-07 15:19:25 +0200

    Add project files.

---
+++ AgeCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Type your birth year:");
+
+        // Read and parse the birth year input
+        int birthYear = int.Parse(Console.ReadLine());
+
+        // Calculate the user's age
+        int currentYear = DateTime.Now.Year;
+        int age = currentYear - birthYear;
+
+        // Output the result
+        Console.WriteLine($"You are {age} years old.");
+    }
+}
